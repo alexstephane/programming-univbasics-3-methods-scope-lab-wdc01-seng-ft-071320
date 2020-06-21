96163dd8206b5dbec6a6a8286dbfc8aad74c844b
Alex Ngundji <alexngundji@gmail.com>
2020-06-21 22:07:51 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,21 +7,21 @@ def mario
 phrase = "It's-a me, Mario!"
   puts phrase
 end
-puts mario
+#puts mario
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
-puts toadstool
+#puts toadstool
 
 def link
   the_link  = "It's Dangerous To Go Alone! Take This."
   
   puts the_link
 end
-puts link
+#puts link
 
 def all_phrases
 new = mario
